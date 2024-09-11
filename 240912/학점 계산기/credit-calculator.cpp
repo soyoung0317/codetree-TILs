c8f9681e20c4943062d76b27d09c7b3eb24bb7f4
Sooo2 <136806281+soyoung0317@users.noreply.github.com>
2024-09-12 01:46:19 +0900

    Title: 학점 계산기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240912/학점 계산기/credit-calculator.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int number = 0;
+    cin >> number;
+
+    double degree;
+    double sum = 0.0;
+
+    for (int i = 0; i < number; ++i)
+    {
+        cin >> degree;
+        sum += degree;
+    }
+
+    double result = sum / (double)number;
+    double avg = round(result * 10.0) / 10.0;
+
+    cout << avg << endl;
+
+    if (avg >= 4.0)
+        cout << "Perfect";
+    else if(avg >= 3.0 && avg < 4.0)
+        cout << "Good";
+    else
+        cout << "Poor";
+
+    return 0;
+}
