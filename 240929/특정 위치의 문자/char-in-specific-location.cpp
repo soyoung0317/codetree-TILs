4b01a2a1c91b3dd17d93ebd36a9f89616493af31
Sooo2 <136806281+soyoung0317@users.noreply.github.com>
2024-09-29 22:46:35 +0900

    Title: 특정 위치의 문자, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240929/특정 위치의 문자/char-in-specific-location.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	char word[6] = { 'L', 'E', 'B', 'R', 'O', 'S' };
+
+	int idx = -1;
+
+	// 문자 탐색
+	for (int i = 0; i < 5; i++) {
+		char temp;
+		cin >> temp;
+		if (temp == word[i]) {
+			idx = i;
+		}
+	}
+
+	if (idx == -1)
+		cout << "None" << endl;
+	else
+		cout << idx << endl;
+
+    return 0;
+}
