3d1928475309074a30474e416d680065398124f1
Sooo2 <136806281+soyoung0317@users.noreply.github.com>
2024-11-05 05:04:35 +0900

    Title: 격자에 점 그리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241105/격자에 점 그리기/draw-points-on-grid.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int size, cnt;
+    cin >> size >> cnt; 
+
+    int place[9][9] = {};
+
+    for (int i = 0; i < cnt; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        place[x-1][y-1] = i+1;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            cout << place[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
